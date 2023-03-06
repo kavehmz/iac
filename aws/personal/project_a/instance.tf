df9258bb932503ef6c1d06ac48b235098797ca40
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2023-03-06 09:21:24 +0100

    test

--- aws/personal/project_a/instance.tf
+++ aws/personal/project_a/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_ebs_volume" "example" {
   count             = 1
-  size              = 8
+  size              = 10
   availability_zone = "us-east-1a"
   tags = {
     Name = "HelloWorld 01"
