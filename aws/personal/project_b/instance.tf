f781a20db45763953c9a45576c7a3cb04fed687b
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2023-03-06 11:46:45 +0100

    add plan before apply

--- aws/personal/project_b/instance.tf
+++ aws/personal/project_b/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_ebs_volume" "example" {
   count             = 0
-  size              = 10
+  size              = 12
   availability_zone = "us-east-1a"
   tags = {
     Name = "HelloWorld 02"
